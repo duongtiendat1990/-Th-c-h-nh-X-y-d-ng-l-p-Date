1e3b3bf5c975b0f268654ef0390242e8ba24dfb3
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-04-08 13:59:26 +0700

    'date'

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ this.setMonth = function(month) {
 this.setYear = function(year) {
     this.year  = year;
 };
+this.setDate = function (day,month,year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+};
+this.toString = function () {
+    if (this.day< 10)this.day = '0' + this.day + '/';
+    else this.day = this.day+ '/';
+    if (this.month<10) this.month = '0' + this.month + '/';
+    else this.month = this.month + '/';
+    this.year = this.year + '.';
+    return this.day + this.month + this.year;
+}
 };
 let date = new MyDate(2, 2, 2007);
 let day = date.getDay(); // 2
@@ -36,3 +49,5 @@ date.setDay(10);
 date.setMonth(10);
 date.setYear(2019);
 alert(date.day + "/" + date.month + "/" + date.year);
+date.setDate(8,4,2019);
+alert(date.toString());
